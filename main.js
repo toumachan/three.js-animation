ac9411f3f79d2696879cb31cfc8127a614cf740c
Toma <take.foot.14@i.softbank.jp>
2022-08-14 23:00:12 +0900

    main.js change

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls
 import * as dat from "lil-gui";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
-import texture01 from "./texture/earth.jpg";
-import texture02 from "./texture/star02.png";
-import texture03 from "./texture/Burst01.png";
+import texture01 from "/texture/earth.jpg";
+import texture02 from "/texture/star02.png";
+import texture03 from "/texture/Burst01.png";
 
 //UIデバッグ
 const gui = new dat.GUI();
