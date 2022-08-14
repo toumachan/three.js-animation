2e0200d683cfe4a5a31726bb441d1c0d15587a95
Toma <take.foot.14@i.softbank.jp>
2022-08-14 22:46:13 +0900

    main.js change

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import './style.css'
-import * as THREE from 'three';
-import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
-import * as dat from "lil-gui";
-import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
-import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
+import * as THREE from '/three';
+import { TrackballControls } from '/three/examples/jsm/controls/TrackballControls.js';
+import * as dat from "/lil-gui";
+import { FontLoader } from "/three/examples/jsm/loaders/FontLoader.js";
+import { TextGeometry } from "/three/examples/jsm/geometries/TextGeometry.js";
 
 //UIデバッグ
 const gui = new dat.GUI();
