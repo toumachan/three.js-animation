8b72a65bdcc079907f18e95b841c14f2a68bf562
Toma <take.foot.14@i.softbank.jp>
2022-08-15 21:08:23 +0900

    main.js change

--- main.js
+++ main.js
@@ -78,7 +78,7 @@ scene.add(container);
 let textMesh;
 const textSize = 6;
 const fontLoader = new FontLoader();
-fontLoader.load("./node_modules/three/examples/fonts/droid/droid_sans_mono_regular.typeface.json", function(font) {
+fontLoader.load("./font/droid_sans_mono_regular.typeface.json", function(font) {
   createTextGeometry("U", font, 330);
   createTextGeometry("N", font, 345);
   createTextGeometry("I", font, 0);
